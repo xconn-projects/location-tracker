ca99459a9a6f95de4de498acb4ed1e607ea419c0
Muzzammil Shahid <muzzammil@xconn.io>
2024-07-06 15:47:24 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# location_tracker
+
+Location Tracker
---
+++ android/app/src/main/kotlin/io/xconn/location_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.xconn.location_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
